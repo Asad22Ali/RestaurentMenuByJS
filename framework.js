a9dbbc5499c26161712e258e7d792a1410e9e268
Asad22Ali <asadlatki5@gmail.com>
2022-02-25 19:45:22 +0500

    first commit

---
+++ framework.js
@@ -0,0 +1,5 @@
+const allMe = document.querySelector('.all')
+const breakFast = document.querySelector('.breakfast')
+const lunchShow = document.querySelector('.lunch')
+const shaKes = document.querySelector('.shakes')
+const dinnEr = document.querySelector('.dinner')
